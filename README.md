1b53f72517339f89548e8434090e26e0b4c2d7dc
Parul Gupta <parulguptasmail@gmail.com>
2018-03-08 22:18:57 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# My `#collect`
+llection # My `#collect`
 
 ## Objectives
 
--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+  if block_given?
+    new_collection = []
+    i = 0 
+    while i < collection.length 
+      new_collection << yield(collection[i])
+      i += 1 
+    end
+  end
+  new_collection
+end
 
